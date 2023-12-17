7d9a34924e8d22761d5be72129847c011fed6085
AdrianPilko <50658445+AdrianPilko@users.noreply.github.com>
2023-12-17 11:51:15 +0000

    random galaxy row control star with keypad

--- assemblyCode/game1.asm
+++ assemblyCode/game1.asm
@@ -34,9 +34,13 @@
     ld (starPosRow), a
     ld (starPosCol), a
     ld (galaxyPosRow), a
-    
+    ld (alternateLoopUpdateFlag), a
     ld a, DISPLAY_COLS-1    
     ld (galaxyPosCol), a
+    
+    ; Initialize seed
+    ld a, $33
+    ld (seedValue), a  
 
 mainLoop:      
         
@@ -55,7 +59,10 @@ mainLoop:
     ld b, a 
     ld a, (galaxyPosCol)
     call moveLCDCursorLocation
-    call displayGalaxy    
+    call displayGalaxy  
+
+
+    call checkCollision    
     
     ld a, (starPosRow)          
     ld b, a
@@ -70,28 +77,45 @@ mainLoop:
     call moveLCDCursorLocation
     call displayBlank    
 
+    ld a,(alternateLoopUpdateFlag)
+    cp 0
+    jp z, skipAlternateLoopThisTime
+    
+    xor a 
+    ld (alternateLoopUpdateFlag), a 
+
     ld a, (galaxyPosCol)
     dec a
+    ld (galaxyPosCol), a    
     cp 0
     jp nz, skipResetGalaxyPos
     ld a, DISPLAY_COLS-1
-skipResetGalaxyPos:        
-    ld (galaxyPosCol), a
-    ;ld a, b
-    ;ld (galaxyPosRow), a    
-    
+    ld (galaxyPosCol), a    
+    ;; now chose random row for galaxy
+
+    ld a, (seedValue)  ; Read from address pointed by HL    
+    rrca         ; Rotate A right through carry (pseudo-random due to varying timing)
+    and 1
+    ld (galaxyPosRow), a            
+    jp skipResetGalaxyPos
+skipAlternateLoopThisTime:
+    ld a, 1
+    ld (alternateLoopUpdateFlag), a 
+skipResetGalaxyPos:            
     call keyboardScanInit
     ld a, b
     ld (starPosRow), a
-    
 
-    ld a, (starPosCol)
-    inc a       
+    ld a, (starPosCol)   
+    inc a           
     cp DISPLAY_COLS
     jp nz, skipZeroA
     xor a   
 skipZeroA:    
     ld (starPosCol), a
+    rrca
+    ld (seedValue), a               
+    
     jp mainLoop   
     
     
@@ -126,6 +150,41 @@ waitLoopAfterKeyFound22:
     djnz waitLoopAfterKeyFound11
     
     pop bc
+    ret
+    
+gameOver
+    ld a, 0
+    ld b, 0
+    call moveLCDCursorLocation
+    ld hl, GameOverMessage
+gameOverMessageLoop:         
+    call waitLCD 
+    ld a, (hl)
+    cp $ff
+    jp z, lastInstruction
+    out (lcdRegisterSelectData), a
+    inc hl
+    jp gameOverMessageLoop    
+lastInstruction
+    halt
+    
+checkCollision
+    ; check if row equal
+    ld a, (starPosRow)
+    ld b, a    ; b now stores star position row
+    ld a, (galaxyPosRow)
+    cp b
+    jp nz, skipGameOver
+
+    ; check if column equal
+    ld a, (starPosCol)
+    ld b, a    ; b now stores star position col
+    ld a, (galaxyPosCol)
+    cp b
+    jp nz, skipGameOver 
+    jp gameOver
+skipGameOver    
+    ;; you avoided -- for now!!!
     ret
     
   
@@ -308,7 +367,8 @@ ConvertToASCII_ret:
 
 InitCommandList:
     .db $38,$0e,$01,$06,$ff
-    
+GameOverMessage:
+    .db "Game Over!!!",$ff    
 ;;; ram variables    
     .org RAM_START
 starPosRow
@@ -319,7 +379,10 @@ galaxyPosRow
     .db $00
 galaxyPosCol
     .db $00     
-    
+alternateLoopUpdateFlag
+    .db $00     
+seedValue
+    .db $13
 to_print:
     .dw $0000
 #END
